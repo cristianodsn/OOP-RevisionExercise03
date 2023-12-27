c09f57931d921a5ccdcdbb557a6b50e224399ccb
Cristiano Castiel <cristianoct03@gmail.com>
2023-12-27 14:53:04 -0300

    Exercise finished

---
+++ Rascunho/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student;
+            Console.WriteLine("Enter the three grades of the student:");
+            Console.Write("Stuent's name:");
+            string name = Console.ReadLine();
+            double grade01 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double grade02 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double grade03 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student = new Student() { Name = name, Grade01 = grade01, Grade02 = grade02, Grade03 = grade03 };
+
+            Console.WriteLine();
+
+            if(student.RemainingGrade() == 0.0)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine("APPROVED");                
+            }
+
+            else
+            {
+                Console.WriteLine(student);
+                Console.WriteLine("DISAPROVED");
+                Console.WriteLine("Remaining grade: " + student.RemainingGrade().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rascunho/Student.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Rascunho
+{
+    class Student
+    {
+        public string Name;
+        public double Grade01, Grade02, Grade03;
+
+        public double FinalGrade()
+        {
+            return Grade01 + Grade02 + Grade03;
+        }
+
+        bool Approved()
+        {
+            if (FinalGrade() >= 60.0) return true;
+            return false;
+        }
+
+        public double RemainingGrade()
+        {
+            if (Approved())
+            {
+                return 0.0;
+            }
+            return 60.0 - FinalGrade(); 
+        }
+
+        public override string ToString()
+        {
+            return "Final Grade: " + FinalGrade().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
